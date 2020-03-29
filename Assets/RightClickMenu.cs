d16467cba207c286bf631ef2ac110bae750ea20a
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2020-03-29 17:15:43 +0200

    Created house, and made something better
    idk to be honest

--- Assets/RightClickMenu.cs
+++ Assets/RightClickMenu.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RightClickMenu : MonoBehaviour
+public class RightClickMenu : UI
 {
+    private Camera cam;
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
     public void GatherButton()
     {
-        gameObject.SetActive(false);
+        Event e = Event.current;
+        Ray ray = cam.ScreenPointToRay(e.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit rc))
+        {
+            Debug.Log("Hit " + rc.collider);
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 10);
+        }
+        CloseMenu();
     }
 
     public void OpenMenu()
     {
-        Vector3 offset = new Vector3(-165, 250);
-        transform.position = Input.mousePosition - offset;
+        Vector3 offset = new Vector3(125, -190);
+        transform.position = Input.mousePosition;
         gameObject.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            CloseMenu();
+        }
     }
 }
